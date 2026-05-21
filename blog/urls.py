5e1588f2e2c66e15d6c89fb94a0f1a8ba6765305
MNINS005 <nikhil06012005@gmail.com>
2026-05-21 17:13:50 +0530

    done

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+
 from .import views
+from .views import PostListView , PostDetailView , PostCreateView , PostUpdateView
 urlpatterns = [
-    path('', views.home,name='blog-home'),
+    path('', views.PostListView.as_view(template_name='blog/home.html'), name='blog-home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(template_name='blog/post_detail.html'), name='blog-post-detail'),
+    path('post/new/', views.PostCreateView.as_view(template_name='blog/post_form.html'), name='blog-post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(template_name='blog/post_form.html'), name='blog-post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='blog-post-delete'),
+    path('about/', views.about, name='blog-about'),
+    
 ]
