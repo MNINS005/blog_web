ae0fdbfb992e6203d75e23e6f614d79f6783e79b
MNINS005 <nikhil06012005@gmail.com>
2026-06-10 11:10:11 +0530

    deployment ready

--- web_app/settings.py
+++ web_app/settings.py
@@ -142,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('your-email@gmail.com')
+EMAIL_HOST_USER = os.getenv('your-email')
 EMAIL_HOST_PASSWORD = os.getenv('your-password')
